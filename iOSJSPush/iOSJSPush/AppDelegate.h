1792b72b956ae13eb40b28e52177ebc29de056ef
mugworts <kamaws@163.com>
2016-08-23 15:43:52 +0800

    添加本地推送

---
+++ iOSJSPush/iOSJSPush/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JSPush
+//
+//  Created by HHW on 16/6/30.
+//  Copyright © 2016年 BWF. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ iOSJSPush/iOSJSPush/AppDelegate.m
@@ -0,0 +1,86 @@
+//
+//  AppDelegate.m
+//  JSPush
+//
+//  Created by HHW on 16/6/30.
+//  Copyright © 2016年 BWF. All rights reserved.
+//
+
+#import "AppDelegate.h"
+#import "JPUSHService.h"
+
+@interface AppDelegate ()
+
+@end
+
+@implementation AppDelegate
+
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    
+    
+    if ([[UIDevice currentDevice].systemVersion floatValue] >= 8.0) {
+        //可以添加自定义categories
+        [JPUSHService registerForRemoteNotificationTypes:(UIUserNotificationTypeBadge |
+                                                          UIUserNotificationTypeSound |
+                                                          UIUserNotificationTypeAlert)
+                                              categories:nil];
+    } else {
+        //categories 必须为nil
+        [JPUSHService registerForRemoteNotificationTypes:(UIRemoteNotificationTypeBadge |
+                                                          UIRemoteNotificationTypeSound |
+                                                          UIRemoteNotificationTypeAlert)
+                                              categories:nil];
+    }
+    
+    
+    /*!
+     * @abstract 启动SDK
+     *
+     * @param launchingOption 启动参数.
+     * @param appKey 一个JPush 应用必须的,唯一的标识. 请参考 JPush 相关说明文档来获取这个标识.
+     * @param channel 发布渠道. 可选.
+     * @param isProduction 是否生产环境. 如果为开发状态,设置为 NO; 如果为生产状态,应改为 YES.
+     * @param advertisingIdentifier 广告标识符（IDFA） 如果不需要使用IDFA，传nil.
+     *
+     * @discussion 提供SDK启动必须的参数, 来启动 SDK.
+     * 此接口必须在 App 启动时调用, 否则 JPush SDK 将无法正常工作.
+     */
+
+    [JPUSHService setupWithOption:launchOptions appKey:@"1ff9db3a7370c83a77f66cb0"
+                          channel:@"Test"
+                 apsForProduction:NO
+            advertisingIdentifier:nil];
+    
+    return YES;
+
+}
+
+- (void)application:(UIApplication *)application
+didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)deviceToken {
+    
+    /// Required - 注册 DeviceToken
+    [JPUSHService registerDeviceToken:deviceToken];
+}
+
+- (void)application:(UIApplication *)application didReceiveRemoteNotification:(NSDictionary *)userInfo {
+    
+    // Required,For systems with less than or equal to iOS6
+    [JPUSHService handleRemoteNotification:userInfo];
+}
+
+- (void)application:(UIApplication *)application didReceiveRemoteNotification:(NSDictionary *)userInfo fetchCompletionHandler:(void (^)(UIBackgroundFetchResult))completionHandler {
+    
+    // IOS 7 Support Required
+    [JPUSHService handleRemoteNotification:userInfo];
+    completionHandler(UIBackgroundFetchResultNewData);
+}
+
+- (void)application:(UIApplication *)application didFailToRegisterForRemoteNotificationsWithError:(NSError *)error {
+    //Optional
+    NSLog(@"did Fail To Register For Remote Notifications With Error: %@", error);
+}
+
+
+
+@end
